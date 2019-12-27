2f694c83c4d15edf924bbc93c8d873a7d4d27e32
Steven Suh <stevenesuh@gmail.com>
2019-12-26 22:17:55 -0800

    add json schema to validate common schemas between client and server

--- client/src/scripts/services/api.js
+++ client/src/scripts/services/api.js
@@ -1,10 +1,24 @@
 import axios from "axios";
 import queryString from "query-string";
+import ajv from "ajv";
+
+import GetLoginStatusRequestSchema from "shared/schema/user/get-login-status/request.json";
+import GetLoginStatusResponseSchema from "shared/schema/user/get-login-status/response.json";
+import ErrorSchema from "shared/schema/common/error.json";
 
 import { addSnackbar } from "scripts/components/snackbar/actions";
 
 import { API_ROUTES, DEFAULT_PAGE_LIMIT, ERROR_MSGS } from "defs";
 import { TYPES } from "scripts/components/snackbar/defs";
+
+const validator = new ajv({
+  allErrors: true,
+  schemas: [
+    ErrorSchema,
+    GetLoginStatusRequestSchema,
+    GetLoginStatusResponseSchema
+  ]
+});
 
 export const catchApiError = dispatch => error => {
   if (!error.response) {
@@ -18,7 +32,7 @@ export const catchApiError = dispatch => error => {
   } else {
     error.response.data = {
       msg: message,
-      error: true,
+      error: true
     };
   }
 
@@ -26,10 +40,25 @@ export const catchApiError = dispatch => error => {
   return error.response;
 };
 
-export const checkLogin = dispatch => async () => {
+const validateCheckLoginRequest = validator.getSchema(
+  "shared/schema/user/get-login-status/request.json"
+);
+const validateCheckLoginResponse = validator.getSchema(
+  "shared/schema/user/get-login-status/response.json"
+);
+export const checkLogin = dispatch => async (data = {}) => {
+  let validation = validateCheckLoginRequest(data);
+  if (!validation) {
+    throw new Error("checkLogin request is invalid with: " + data);
+  }
   const res = await axios
-    .get(API_ROUTES.USER.LOGIN)
+    .get(API_ROUTES.USER.LOGIN, data)
     .catch(catchApiError(dispatch));
+
+  validation = validateCheckLoginResponse(data);
+  if (!validation) {
+    throw new Error("checkLogin response is invalid with: " + data);
+  }
   return res.data;
 };
 
@@ -37,7 +66,7 @@ export const submitLogin = dispatch => async (email, password) => {
   const res = await axios
     .post(API_ROUTES.USER.LOGIN, {
       email,
-      password,
+      password
     })
     .catch(catchApiError(dispatch));
   return res.data;
@@ -48,7 +77,7 @@ export const submitRegister = dispatch => async (name, email, password) => {
     .post(API_ROUTES.USER.REGISTER, {
       name,
       email,
-      password,
+      password
     })
     .catch(catchApiError(dispatch));
   return res.data;
@@ -65,14 +94,14 @@ export const createInstitutionLink = dispatch => async (
   publicToken,
   accounts,
   institution,
-  linkSessionId,
+  linkSessionId
 ) => {
   const res = await axios
     .post(API_ROUTES.LINK.CREATE, {
       publicToken,
       accounts,
       institution,
-      linkSessionId,
+      linkSessionId
     })
     .catch(catchApiError(dispatch));
   return res.data;
@@ -87,7 +116,7 @@ export const deleteInstitutionLink = dispatch => async institutionLinkId => {
 
 export const getTransactions = dispatch => async (
   userId,
-  { limit = DEFAULT_PAGE_LIMIT, offset = 0, ...params } = {},
+  { limit = DEFAULT_PAGE_LIMIT, offset = 0, ...params } = {}
 ) => {
   const search = queryString.stringify({ ...params, limit, offset });
   const res = await axios
