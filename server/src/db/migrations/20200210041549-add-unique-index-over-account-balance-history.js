12666b4c8b8c2b6164be2a1885e61b8f69d0cfb1
Steven Suh <stevenesuh@gmail.com>
2020-02-09 20:29:43 -0800

    Add unique index over accountId and date on account balance history table

---
+++ server/src/db/migrations/20200210041549-add-unique-index-over-account-balance-history.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.addIndex('AccountBalanceHistories', ['accountId', 'date'], {
+      unique: true,
+    }),
+  down: (queryInterface, Sequelize) =>
+    queryInterface.removeIndex('AccountBalanceHistories', 'account_balance_histories_account_id_date'),
+};
+
